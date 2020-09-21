409fb0ec3070bb56f2dda5a34cef2ef687bf9e86
ltta18 <thuylinh7720@gmail.com>
2020-09-21 10:24:54 +0700

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className='App-link'
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="hi"></div>
     </div>
   );
 }
