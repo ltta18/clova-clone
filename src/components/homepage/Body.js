06d2513c478925475d95e96bcbcde80e31f989b2
ltta18 <thuylinh7720@gmail.com>
2020-09-22 13:39:08 +0700

    Add Layout Body Box - Home

---
+++ src/components/homepage/Body.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import ReactJson from "react-json-view";
+import { jsonObj } from '../../data/data';
+
+const body = <><p>TODAY'S MENU!</p><p>BULGOGI</p><p>EGG BACON BURGER</p><p>&FRENCH FRIES</p><p>INFO.</p></>
+
+const Body = ({ isText }) => {
+  return (
+    <Container className="pt-3 pb-3 pl-3 pr-3 home-body">
+    {isText 
+    ? <div className="body-desc-text desc-text mt-0" style={{ fontWeight: 400 }}>{body}</div>
+    : <ReactJson
+      src={jsonObj}
+      theme="rjv-default"
+      displayDataTypes={false}
+      enableClipboard={false}
+      />
+    }
+    </Container>
+  );
+};
+
+export default Body;
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,10 +1,16 @@
-import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Button, OverlayTrigger, Tooltip, Form } from "react-bootstrap";
+import Body from "../../components/homepage/Body";
 import CarouselImg from "../../components/homepage/CarouselImg";
-import JsonView from "../../components/homepage/JsonView";
 import "./index.css";
 
 const HomePage = () => {
+  const [ isText, setIsText ] = useState(true)
+
+  useEffect(() => {
+    document.getElementById('defaultChecked').focus()
+  }, [])
+
   return (
     <>
       <Container className="position-relative" style={{ marginTop: '200px' }}>
@@ -13,27 +19,48 @@ const HomePage = () => {
       </Container>
       <Container className="rounded bg-light position-relative pr-5 pl-5 pt-5 pb-5">
         <Row>
-          <div style={{ fontSize: '1.3rem' }}>Try it now</div>
+          <div className="try-it">Try it now</div>
         </Row>
         <Row className="mt-5 mb-4 button-container">
-          <Button className="custom-button" title="General OCR">General OCR</Button>
+          <Button id='defaultChecked' className="custom-button" title="General OCR" >General OCR</Button>
           <Button className="custom-button" title="Receipts">Receipts</Button>
           <Button className="custom-button" title="Credit Cards">Credit Cards</Button>
           <Button className="custom-button" title="Business Licenses">Business Licenses</Button>
         </Row>
-        <Row className="">
+        <Row className="body-desc-text desc-text mb-4 mt-0">
           <div>Basic model that can recognize Korean and English.</div>
           <div>It extracts text accurately even in images with severe distortion or complexity, and has excellent handwriting recognition.</div>
         </Row>
         <Row>
-          <Col>
+          <Col className="pl-0">
             <CarouselImg />
           </Col>
-          <Col>
-            <Row></Row>
-            <JsonView />
+          <Col className="pr-0" style={{ border: '1px solid silver' }}>
+            <Row className="pt-3 pb-2 mr-0" style={{ borderBottom: '1px solid silver' }}>
+              <button className="body-button pl-3" onClick={() => setIsText(true)} title="Text">Text</button>
+              <button className="body-button pl-3" onClick={() => setIsText(false)} title="Json">Json</button>
+            </Row>
+            <Row><Body isText={isText} /></Row>
+            <Row className="mr-0 " style={{ borderTop: '1px solid silver' }}>
+              <Row className="pt-3 pb-3 pl-3 pr-3 ml-0" style={{ borderRight: "1px solid silver" }}>
+                <OverlayTrigger
+                  placement="top"
+                  overlay={
+                    <Tooltip id="tooltip-top">
+                      Copy
+                    </Tooltip>
+                  }
+                >
+                  <div className="copy-icon"></div>
+                </OverlayTrigger>
+              </Row>
+              <Col>
+                
+              </Col>
+            </Row>
           </Col>
         </Row>
+        
       </Container>
     </>
   );
